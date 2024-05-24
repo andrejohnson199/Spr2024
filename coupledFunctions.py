130341f9b958f042b07f56b7b46427396e80e7d5
Andre Johnson <166755233+andrejohnson199@users.noreply.github.com>
2024-05-24 12:15:44 -0400

    Fully working
    Only prints through 0p1/2, still works for other orbitals

--- coupledFunctions.py
+++ coupledFunctions.py
@@ -3,6 +3,7 @@
 #Andre Johnson
 
 import numpy as np
+from tabulate import tabulate
 from sympy.physics.wigner import wigner_3j
 
 #Ill right out all the different functions 
@@ -22,6 +23,9 @@ class twoCoupled:
         self.J = J
         self.T = T
 
+#GETTING RID OF MS
+#I BELIEVE IT IS JUST DOUBLE THE AMOUNT I HAVE TO STORE
+
 #function to fill all single particle states
 #returns an array of all single particle states
 def fillSingleStates(N):
@@ -32,52 +36,128 @@ def fillSingleStates(N):
                 j = l+1/2
                 if l == 0:
                     nljarr = np.append(nljarr,[coupled(n,l,j,0)])
-                    nljarr = np.append(nljarr,[coupled(n,l,j,1)])
+                    #nljarr = np.append(nljarr,[coupled(n,l,j,1)])
                 else:
                     for i in range(2):
                         nljarr= np.append(nljarr,[coupled(n,l,j,0)])
-                        nljarr = np.append(nljarr,[coupled(n,l,j,1)])
+                        #nljarr = np.append(nljarr,[coupled(n,l,j,1)])
                         j-=1        
     return nljarr
 
 def fillTwoStates(oneStates):
     numSize = oneStates.size
     #Setting T=1, like nucleons
-    T=1
+    #T=1
     twoArr = np.array([],dtype=twoCoupled)
     for i in range(numSize):
         a = oneStates[i]
         for k in range(numSize):
-            if i!=k:
-                # a and b are the single particle states
-                b = oneStates[k]
-                #need to get the J coupling
-                for J in range(int(abs(a.j-b.j)),int(a.j+b.j)+1):
-                    twoArr = np.append(twoArr,[twoCoupled(i,k,J,T)])
+            # if i!=k:
+            #     # a and b are the single particle states
+            #     b = oneStates[k]
+            #     #need to get the J coupling
+            #     for J in range(int(abs(a.j-b.j)),int(a.j+b.j)+1):
+            #         #T=0,1
+            #         twoArr = np.append(twoArr,[twoCoupled(i,k,J,0)])
+            #         twoArr = np.append(twoArr,[twoCoupled(i,k,J,1)])
+
+            #WITHOUT MS
+            # a and b are the single particle states
+            b = oneStates[k]
+            #need to get the J coupling
+            for J in range(int(abs(a.j-b.j)),int(a.j+b.j)+1):
+                #T=0,1
+                twoArr = np.append(twoArr,[twoCoupled(i,k,J,0)])
+                twoArr = np.append(twoArr,[twoCoupled(i,k,J,1)])
     return twoArr
 
 def fillPotential(two,one):
     vMat = np.zeros((len(two),len(two)))
+    #Table
+    table = np.array(['abcd','J','T','V'])
     #Printing index
     i = 0
+    aname = ''
+    bname = ''
+    cname = ''
+    dname = ''
     for row in range(len(vMat)):
         a = one[two[row].a]
         b = one[two[row].b]
         J1 = two[row].J
         T1 = two[row].T
+
+        #Printing name
+        if a.l==0:
+            aname = '0s1/2'
+            aname = 1
+        elif a.j==1.5:
+            aname = '0p3/2'
+            aname = 2
+        else:
+            aname = '0p1/2'
+            aname = 3
+        if b.l==0:
+            bname = '0s1/2'
+            bname = 1
+        elif b.j==1.5:
+            bname = '0p3/2'
+            bname = 2
+        else:
+            bname = '0p1/2'
+            bname = 3
         for col in range(len(vMat)):
             c = one[two[col].a]
             d = one[two[col].b]
             J2 = two[col].J
             T2 = two[col].T
 
+            #Printing name
+            if c.l==0:
+                cname = '0s1/2'
+                cname = 1
+            elif c.j==1.5:
+                cname = '0p3/2'
+                cname = 2
+            else:
+                cname = '0p1/2'
+                cname = 3
+            if d.l==0:
+                dname = '0s1/2'
+                dname = 1
+            elif d.j==1.5:
+                dname = '0p3/2'
+                dname = 2
+            else:
+                dname = '0p1/2'
+                dname = 3
+
             #Parity, J, and T need to match
             #Nontrivial elements
+            #Not printing the zeros
             if (a.l-c.l)%2 == (b.l-d.l)%2 and J1 == J2 and T1 == T2:
                 vMat[row][col] = vElement(a,b,c,d,J1,T1)
-                if i<10:
-                    print(vMat[row][col],'Element: ',row,col,' a: ',a.n,a.l,a.j,' b: ',b.n,b.l,b.j,' c: ',c.n,c.l,c.j,' d: ',d.n,d.l,d.j,' J: ',J1,' T: ',T1)
-                    i+=1
+
+                #Do not need to print redundant states
+                if abs(vMat[row][col])>0 and cname<=dname and aname<=bname: 
+                    #print(vMat[row][col],'Element: ',row,col,' a: ',a.n,a.l,a.j,' b: ',b.n,b.l,b.j,' c: ',c.n,c.l,c.j,' d: ',d.n,d.l,d.j,' J: ',J1,' T: ',T1)
+
+                    #New Printing
+                    #print(vMat[row][col],'Element:',row,col,' abcd: ',orbits[0],orbits[1],orbits[2],orbits[3])
+                    #print(vMat[row][col],'Element:',row,col,' abcd: ',aname,bname,cname,dname,' J: ',J1,' T: ',T1)
+                    #print(aname,bname,cname,dname, ' ',J1,T1,' ',vMat[row][col])
+
+                    #Adding to the table
+                    tNum = 1000*aname+100*bname+10*cname+dname
+                    table = np.vstack([table,[tNum,J1,T1,vMat[row][col]]])
+    #Print to terminal
+    print(tabulate(table,headers='firstrow'))
+    print('1=0s1/2, 2=0p3/2, 3=0p1/2')
+    # #Print to output file
+    # #Uncomment if you want
+    # with open("output.txt", "a") as f:
+    #     print(tabulate(table,headers='firstrow'),file=f)
+    #     print('1=0s1/2, 2=0p3/2, 3=0p1/2',file=f)
     return vMat
 
 
--- coupledMain.py
+++ coupledMain.py
@@ -12,18 +12,8 @@ Nmax = 1
 #Get all single particle states
 singleStates = fillSingleStates(Nmax)
 
-
 #Getting all two particle states
 twoStates = fillTwoStates(singleStates)
-
-# #testing vElement
-# #All states in 0s with J=0 and T=1, v=-1
-a = singleStates[0]
-b = singleStates[1]
-c = singleStates[0]
-d = singleStates[1]
-vTest = vElement(a,b,c,d,0,1)
-print(vTest)
 
 #Potential Matrix
 vMatrix = fillPotential(twoStates,singleStates)
